ce740be31c7636ff2c40e3665328b644529a97ac
DanMoll <dani948d@edu.easj.dk>
2019-02-24 20:41:33 +0100

    Opsatte Class Library og skal til at lave unittest.

---
+++ BilletOpgave/BilletLibrary/BilletDLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bil
+{
+    public class BilletDLL
+    {
+        class BilContent
+        {
+            private string _nummerplade;
+            private DateTime _dato;
+
+            public DateTime Dato
+            {
+                get { return _dato; }
+                set { _dato = value; }
+            }
+
+            public string Nummerplade
+            {
+                get { return _nummerplade; }
+                set { _nummerplade = value; }
+            }
+
+            public decimal Pris()
+            {
+                return 240;
+            }
+
+            public string Køretøj()
+            {
+                return "Bil";
+            }
+
+        }
+    }
+}
